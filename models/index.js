0bede7c9dcc0e22756750bbb80a9484b667fc23f
Nate Hogan <natek.hogan@gmail.com>
2021-04-19 19:46:30 -0600

    added code

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Post = require("./Post");
 const Vote = require('./Vote');
+const Comment = require('./Comments');
 
 // create associations
 User.belongsToMany(Post, {
