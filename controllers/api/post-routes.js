6357b22393fd51d73be1178084b53fa0843e754c
Nate Hogan <natek.hogan@gmail.com>
2021-04-20 18:49:10 -0600

    added login and logout functions

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -13,7 +13,6 @@ router.get('/', (req, res) => {
         'created_at',
         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count'] 
       ],
-      order: [['created_at', 'DESC']],
       include: [
         {
           model: Comment,
@@ -51,6 +50,14 @@ router.get('/:id', (req, res) => {
     ],
     include: [
       {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
         model: User,
         attributes: ['username']
       }
@@ -74,7 +81,7 @@ router.post('/', (req, res) => {
     Post.create({
       title: req.body.title,
       post_url: req.body.post_url,
-      user_id: req.body.user_id
+      user_id: req.session.user_id
     })
     .then(dbPostData => res.json(dbPostData))
     .catch(err => {
@@ -85,7 +92,7 @@ router.post('/', (req, res) => {
 
 router.put('/upvote', (req, res) => {
   // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote })
+  Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
     .then(updatedPostData => res.json(updatedPostData))
     .catch(err => {
       console.log(err);
